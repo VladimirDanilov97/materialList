300301aaba6b5e6012be42ce20cbdce8c87ae83c
VladmirDanilov97 <dan1997ford@yandex.ru>
2022-12-26 07:30:16 +0300

    class Db

--- models.py
+++ models.py
@@ -1,25 +1,48 @@
 from sqlalchemy import Table, MetaData, Column, String, Integer, ForeignKey, create_engine
-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+Base = declarative_base()
 metadata = MetaData()
 
 engine = create_engine(f"sqlite:///material_list.db")
 
-products = Table('products', metadata,
-                Column('product_id', Integer, primary_key=True),
-                Column('product_name', String, ),
-                Column('part_of', ForeignKey('products.product_id')))
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column('id', Integer, primary_key=True)
+    name = Column('product_name', String, )
+    part_of = Column('part_of', ForeignKey('products.id'), nullable=True)
 
-sheet = Table('sheet', metadata,
-               Column('sheet_id', Integer, primary_key=True),
-               Column('width', Integer),
-               Column('length', Integer),
-               Column('thickness', Integer),
-               Column('material', String),
-               Column('amount', Integer),
-               Column('product', ForeignKey('products.product_id')))
+    def __init__(self, id, name, part_of=id) -> None:
+        self.id = id
+        self.name = name,
+        self.part_of = part_of
+    
+    def __repr__(self):
+        return f'{self.name}'
+
+# products = Table('products', metadata,
+#                 Column('product_id', Integer, primary_key=True),
+#                 Column('product_name', String, ),
+#                 Column('part_of', ForeignKey('products.product_id')))
+
+# sheet = Table('sheet', metadata,
+#                Column('sheet_id', Integer, primary_key=True),
+#                Column('width', Integer),
+#                Column('length', Integer),
+#                Column('thickness', Integer),
+#                Column('material', String),
+#                Column('amount', Integer),
+#                Column('product', ForeignKey('products.product_id')))
 
 
 
 if __name__ == '__main__':
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    p = Product(id=1, name='manhole')
+    session.add(p)
+
+    session.commit()
