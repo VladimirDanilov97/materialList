96882c966be814817de88191b27553d89cb36cc8
Vladimir <dan1997ford@yandex.ru>
2022-12-27 08:26:37 +0300

    add columns

--- models.py
+++ models.py
@@ -1,48 +1,61 @@
 from sqlalchemy import Table, MetaData, Column, String, Integer, ForeignKey, create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-Base = declarative_base()
+
 metadata = MetaData()
 
 engine = create_engine(f"sqlite:///material_list.db")
 
-class Product(Base):
-    __tablename__ = 'products'
-    id = Column('id', Integer, primary_key=True)
-    name = Column('product_name', String, )
-    part_of = Column('part_of', ForeignKey('products.id'), nullable=True)
+products = Table('products', metadata,
+                Column('product_id', Integer, primary_key=True),
+                Column('product_name', String, ),
+                )
 
-    def __init__(self, id, name, part_of=id) -> None:
-        self.id = id
-        self.name = name,
-        self.part_of = part_of
-    
-    def __repr__(self):
-        return f'{self.name}'
+sheet = Table('sheets', metadata,
+               Column('sheet_id', Integer, primary_key=True),
+               Column('width', Integer),
+               Column('length', Integer),
+               Column('thickness', Integer),
+               Column('material', String),
+               Column('amount', Integer),
+               )
 
-# products = Table('products', metadata,
-#                 Column('product_id', Integer, primary_key=True),
-#                 Column('product_name', String, ),
-#                 Column('part_of', ForeignKey('products.product_id')))
+tube = Table('tubes', metadata,
+               Column('sheet_id', Integer, primary_key=True),
+               Column('diameter', Integer),
+               Column('thickness', Integer),
+               Column('length', Integer),
+               Column('material', String),
+               Column('amount', Integer),
+               )
 
-# sheet = Table('sheet', metadata,
-#                Column('sheet_id', Integer, primary_key=True),
-#                Column('width', Integer),
-#                Column('length', Integer),
-#                Column('thickness', Integer),
-#                Column('material', String),
-#                Column('amount', Integer),
-#                Column('product', ForeignKey('products.product_id')))
+nuckle = Table('nuckles', metadata,    
+               Column('nuckle_id', Integer, primary_key=True),
+               Column('diameter', Integer), 
+               Column('material', String),
+               )
 
+bolt = Table('bolts', metadata,    
+               Column('bolt_id', Integer, primary_key=True),
+               Column('diameter', Integer), 
+               Column('length', Integer), 
+               Column('material', String),
+               )
+
+flange = Table('flange', metadata,    
+               Column('flange_id', Integer, primary_key=True),
+               Column('diameter', Integer), 
+               Column('pressure', Integer),
+               Column('inner diameter', Integer),
+               Column('pressure', Integer),
+               Column('material', String),
+               )
 
 
 if __name__ == '__main__':
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(engine)
-    session = Session()
-
-    p = Product(id=1, name='manhole')
-    session.add(p)
-
-    session.commit()
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
+    stmt = products.insert().values(product_name='foo')
+    with engine.connect() as conn:
+        conn.execute(stmt)
+       
+   
